2a9b75ab5356d6b308d425cca8d9fd00d8aac962
rashiccv <32409006+merashi@users.noreply.github.com>
2019-03-06 11:19:15 +0530

    factorial fabonacci

--- src/main/Recursion.java
+++ src/main/Recursion.java
@@ -1,7 +1,31 @@
 package main;
 
 public class Recursion {
-void countDown(int number){
-
+static int factorial(int number) {
+   if(number==1){
+       return 1;
+   }
+   else{
+       return number * factorial(--number);
+   }
 }
+static int fabonacci(int num){
+    if(num == 0){
+        return 0;
+    }
+    else if(num == 1){
+        return 1;
+    }
+    else {
+        return fabonacci(num-1)+fabonacci(num-2);
+    }
+}
+    public static void main(String[] args) {
+   int c=0;
+        System.out.println(factorial(4));
+        for (int i = 0; i < 5; i++) {
+            System.out.println(fabonacci(c));
+            c++;
+        }
+    }
 }
